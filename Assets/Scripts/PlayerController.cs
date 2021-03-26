3f32b021326f863ecb72d129e3a2bf8e7fa9c960
Luke Alexander <23AlexL@live.elida.k12.oh.us>
2021-03-26 08:53:43 -0400

    Finished play area and made collectables

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,10 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+
     private Rigidbody rb;
+
     private float movementX;
     private float movementY;
 
@@ -15,18 +18,19 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void OnMove(InputValue movementValue)
+    private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+
         movementX = movementVector.x;
-        movementY = movementVector.y; 
+        movementY = movementVector.y;
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(movementVector);
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
     }
 
 }
-
-
